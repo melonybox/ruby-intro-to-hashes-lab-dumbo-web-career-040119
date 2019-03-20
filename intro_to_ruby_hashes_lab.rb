1be5a83f7874a8084af6ddca0350c70d811f7f5f
Gino Desangles <blushingbox@yahoo.com>
2019-03-20 08:10:00 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -21,7 +21,7 @@ end
 def monopoly_with_third_tier
 	monopoly = {:railroads => {
 	  pieces: 4, :names => {
-	    :reading_raildroad => {},
+	    :reading_railroad => {},
 	    :pennsylvania_railroad => {},
 	    :b_and_o_railroad => {},
 	    :shortline => {}
